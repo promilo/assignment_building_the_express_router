f12bfa1d2d32b50e0bb77adc99ed6aa802e67dc7
promilo <milind.shah@mail.utoronto.ca>
2017-09-30 18:43:47 -0400

    started doing regex operations

--- index.js
+++ index.js
@@ -4,11 +4,15 @@ var express = require('./lib/express');
 // 2. Create an application with the
 // returned function
 var app = express();
+console.log("app", app)
 
 // 3. Create routes via the app object
 app.get('/', (req, res) => {
-  console.log(req);
   res.end('Hi world!\n');
+});
+
+app.get('/parsed/:1', (req, res) => {
+  res.end('its [parsed]');
 });
 
 // 4. Start up a server with app.listen
